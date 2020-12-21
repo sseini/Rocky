bf10bdf94877fd97ac81f3d61a6fcfdd45ef8071
Saley SEINI <sseini@gmail.com>
2020-12-21 01:36:23 +0100

    Initial Setup

---
+++ Controllers/ApplicationTypeController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Rocky.Data;
+using Rocky.Models;
+
+namespace Rocky.Controllers
+{
+    public class ApplicationTypeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ApplicationTypeController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationType> objList = this._db.ApplicationType;
+            return View(objList);
+        }
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ApplicationType obj)
+        {
+            this._db.ApplicationType.Add(obj);
+            this._db.SaveChanges();
+            //return View();
+            return RedirectToAction("Index");
+        }
+
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ApplicationType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                this._db.ApplicationType.Update(obj);
+                this._db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.ApplicationType.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            //if (ModelState.IsValid)
+            //{
+            this._db.ApplicationType.Remove(obj);
+            this._db.SaveChanges();
+            return RedirectToAction("Index");
+            //    }
+            //    return View(obj);
+        }
+    }
+}
---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Rocky.Data;
+using Rocky.Models;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = this._db.Category;
+            return View(objList);
+        }
+
+        //GET - EDIT
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                this._db.Category.Add(obj);
+                this._db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                this._db.Category.Update(obj);
+                this._db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            //if (ModelState.IsValid)
+            //{
+                this._db.Category.Remove(obj);
+                this._db.SaveChanges();
+                return RedirectToAction("Index");
+        //    }
+        //    return View(obj);
+        }
+    }
+}
